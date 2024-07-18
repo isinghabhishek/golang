38672c0109b09dcfb6ba649c6f6269d0fa716e41
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2024-07-18 21:58:28 +0530

    add: simple_value

---
+++ 2_simple_values/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+	// int
+	fmt.Println(1)
+
+	//string
+	fmt.Println("Hello World")
+
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// floats
+	fmt.Println(7.14)
+	fmt.Println(2.4)
+}
