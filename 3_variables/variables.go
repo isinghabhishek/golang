9cde0c0802a109b44398b9b86667fff3072d6e05
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2024-07-19 21:30:49 +0530

    add: Variables & constants

---
+++ 3_variables/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string = "golang"
+	// var name = "golang"
+	var age bool = true
+	fmt.Println(age)
+
+	// shorthand syntax
+	name := "golang"
+
+	fmt.Println(name)
+}
---
+++ 4_constants/constants.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+}
+
+const (
+	pi   = 7.14
+	host = "localhost8080"
+)
